f7057823e533e3616af476317f373187ecf8dd72
mustapha essalih <mustaphaessalih1937@gmail.com>
2024-01-31 11:51:17 +0100

    .

--- Module 00/ex05/Program.java
+++ Module 00/ex05/Program.java
@@ -1,158 +1,146 @@
 import java.util.Scanner;
 
-/**
- * Program
- */
 public class Program {
-
-
+    
     private static int numberOfStudents;
     private static String[] listOfStudents;
     private static boolean[][] classes;
     private static int[][][] attendance;
     private static   String[] daysOfWeek = {"MO" , "TU" , "WE" , "TH" , "FR" , "SA" , "SU"};
-        
+    private static String strTime ; 
+    private static String strDate ; 
+    private static String strDay ;
+    private static String nameOfStudent ;
+    private static String strStatus ;
 
-    public static void main(String[] args) {
-        
-        getListOfStudents();
-        
+
+    
+    public static void main(String[] args) 
+    {
+        getListOfStudents();    
+
         getClasses();
-
         createAttendance();
-        // split by scanner
+        
         printTable();
     }
 
 
-    private static void printTable()
+
+    private static void getListOfStudents() 
     {
-        // time
-        // string day
-        // date
-        System.out.printf("%10s", " ");
-        for (int i = 0; i < classes.length; i++) 
+        Scanner scanner = new Scanner(System.in);
+        listOfStudents = new String[10];
+        numberOfStudents = 0;
+         
+        while (numberOfStudents < 10) 
         {
-            for (int j = 0; j < classes[j].length; j++) 
-            {
-                if (classes[i][j] == true) 
-                {
-                    System.out.printf("%1d:00%3s%3d|", j , getDayFromIndex(i % 7) , i );
-                }    
-            }    
+            String studentName = getStudentName(numberOfStudents , scanner );
+            if(studentName == null)
+                break;
+            listOfStudents[numberOfStudents] = studentName;
+            numberOfStudents++;             
         }
-        System.out.println();
         
-        for (int i = 0; i < listOfStudents.length; i++) 
+        if (numberOfStudents == 10) 
+            System.out.println(".");
+    }
+
+
+    private static void getClasses()
+    {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        int numberOfClasses;
+        int indexOfDay;
+        int time;
+        
+                    //     30days 6h
+        classes = new boolean[30][6];
+         
+        numberOfClasses = 0;
+        System.out.print("-> ");
+        while (!(input = scanner.nextLine()).equals("."))  
         {
-            System.out.printf("%10s" , listOfStudents[i]);
-            for (int j = 0; j < classes.length; j++) 
+            
+            while (input.length() == 0) // if just enter enter
             {
-                for (int x = 0; x < classes[j].length; x++) 
-                {
-                    if (classes[j][x] == true) 
-                    {
-                        System.out.printf("%10d|" , attendance[j][x][i]);
-                    }    
-                }    
+                System.out.println("you entered empty arg");
+                System.out.print("-> ");
+                input = scanner.nextLine();
+                if (input.equals(".")) 
+                    return ;    
             }
-            System.out.println();
+            
+            Scanner splitedInput = new Scanner(input).useDelimiter("\\s+");
+            
+            inputClassesFromUser(splitedInput , scanner , 2);
+            
+            time = getTime(strTime);
+            indexOfDay = getIndexOfDay(strDay);
+            while (time == -1 || indexOfDay == -1) 
+            {
+                inputClassesFromUser(splitedInput , scanner , 2);
+                time = getTime(strTime);
+                indexOfDay = getIndexOfDay(strDay);
+            }
+            int rtn = fillClasses(time , indexOfDay , 0);
+            
+            if (rtn != 0) 
+                numberOfClasses++;   
 
+            if(numberOfClasses == 10)
+                break;
+            System.out.print("-> ");
         }
-
     }
 
     private static void createAttendance()
     {
         String input;
+        int time;
+        int status;
+        int date;
+        int indexOfDay;
+        int indexOfStudnet;
 
         attendance = new int[30][6][numberOfStudents];
         
         Scanner scanner = new Scanner(System.in); // split by scanner
-
+        
         System.out.print("-> ");
+        //  inifinite loop because can user want to edit status of attendace
         while (!(input = scanner.nextLine()).equals(".")) 
         {
-            String[] splitedInput = splitString(input, 4);
-
-            while (splitedInput[0] == null) {
-                System.out.println("enter 4 args.");
+            while (input.length() == 0) // if just enter enter
+            {
+                System.out.println("you entered empty arg");
                 System.out.print("-> ");
                 input = scanner.nextLine();
-                splitedInput = splitString(input, 4);
+                if (input.equals(".")) 
+                    return ;    
             }
-
-            String studentName = splitedInput[0];
-            int indexOfStudnet = getIndexOfStudent(studentName);
-            int time = getTime(splitedInput[1]);
-            int date = checkIfDayIsExistInWeek(toDigit(splitedInput[2]) , time);
-            int status = getStatus(splitedInput[3]);
             
-
+            Scanner splitedInput = new Scanner(input).useDelimiter("\\s+");
+            
+            inputClassesFromUser(splitedInput , scanner , 4);
+            time = getTime(strTime);
+            date = checkIfDayIsExistInWeek(toDigit(strDate) , time);
+            indexOfStudnet = getIndexOfStudent(nameOfStudent);
+            status = getStatus(strStatus);
+            
             while (indexOfStudnet == -1 || time == -1 || date == -1 || status == 0 ) 
             {
-                System.out.println("attence input not correct");
-                
-                System.out.print("-> ");
-                input = scanner.nextLine();
-                if (input.equals(".")) 
-                    return ;
-                splitedInput = splitString(input, 4);
-                indexOfStudnet = getIndexOfStudent(splitedInput[0]);
-                time = getTime(splitedInput[1]);
-                date = checkIfDayIsExistInWeek(toDigit(splitedInput[2]) , time);
-                status = getStatus(splitedInput[3]);
+                inputClassesFromUser(splitedInput , scanner , 4);
+                time = getTime(strTime);
+                date = checkIfDayIsExistInWeek(toDigit(strDate) , time);
+                indexOfStudnet = getIndexOfStudent(nameOfStudent);
+                status = getStatus(strStatus);
             }
-            System.out.print("-> ");
             attendance[date - 1][time - 1][indexOfStudnet] = status;
-        }
-
-    }
-
-     
-    private static void getClasses()
-    {
-        Scanner scanner = new Scanner(System.in);
-        String input;
-                    //     30days 6h
-        classes = new boolean[30][6];
-        // loop on 4 weeks
-        System.out.print("-> ");
-
-        int numberOfClasses = 0;
-        while (!(input = scanner.nextLine()).equals(".")) 
-        {
-            String[] splitedStr = splitString(input , 2);
-    
-            while (splitedStr == null) {
-                System.out.println("enter 2 args.");
-                System.out.print("-> ");
-                input = scanner.nextLine();
-                splitedStr = splitString(input, 2);
-            }
-            int time = getTime(splitedStr[0]);
-            
-            int indexOfDay = getIndexOfDay(splitedStr[1]);
-    
-            while (time == -1 || indexOfDay == -1) 
-            {
-                System.out.println("error in time or day");
-                System.out.print("-> ");
-                input = scanner.nextLine();
-                if (input.equals(".")) 
-                    return ;
-                splitedStr = splitString(input , 2);
-                time = getTime(splitedStr[0]);
-                indexOfDay = getIndexOfDay(splitedStr[1]);
-            }
-            
-            numberOfClasses = fillClasses(time , indexOfDay , numberOfClasses);
-            if(numberOfClasses == 10)
-                break;
             System.out.print("-> ");
+             
         }
-
-       
     }
 
     private static int fillClasses(int time , int indexOfDay , int numberOfClasses)
@@ -162,7 +150,7 @@ public class Program {
         // index of monday in week is 0
         
         // we search for index of day entered by user in table to fill all days in  month 
-
+         
         int startDayOfMonth = 1; // TU it is the start day in septembere 2020
         int firstDayOfMonth;
         
@@ -182,111 +170,173 @@ public class Program {
             {
                 classes[i][time - 1] = true;   
                  
-                numberOfClasses++;
+                numberOfClasses++; // because increment 4 times
             }
         }
         
-        return numberOfClasses;
+        return numberOfClasses == 4 ? 1 : 0;
     }
 
-    private static void getListOfStudents() 
+    private static void printTable()
     {
-        Scanner scanner = new Scanner(System.in);
-        String input;
-         
-        listOfStudents = new String[10];
-        numberOfStudents = 0;
-        System.out.print("-> ");
-        while (!(input = scanner.nextLine()).equals(".") && numberOfStudents < 10)  
+        System.out.printf("%10s", " ");
+        for (int i = 0; i < classes.length; i++) 
         {
-             
-            while(input.length() > 10 || input.length() == 0  || includeEspace(input))
+            for (int j = 0; j < classes[j].length; j++) 
             {
+                if (classes[i][j] == true) 
+                {
+                    System.out.printf("%1d:00%3s%3d|", j + 1, getDayFromIndex((i + 1) % 7), i + 1);
+                }    
+            }    
+        }
+        System.out.println();
+        
+        for (int i = 0; i < numberOfStudents; i++) 
+        {
+            System.out.printf("%10s" , listOfStudents[i]);
+            for (int j = 0; j < classes.length; j++) 
+            {
+                for (int x = 0; x < classes[j].length; x++) 
+                {
+                    if (classes[j][x] == true) 
+                    {
+                        if (attendance[j - 1][x][i] == 1) 
+                            System.out.printf("%10s|", "1");
+                        else if (attendance[j - 1][x][i] == -1) 
+                            System.out.printf("%10s|", "-1");
+                        else 
+                            System.out.printf("%10s|", " ");
+                    }    
+                }    
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    private static void inputClassesFromUser(Scanner splitedInput, Scanner scanner , int size) 
+    {
+        String input;
+        String[] timeAndDay = new String[size];
+        int i ;
+        i = 0;
+        while (i < size + 1) 
+        {
+            boolean hasNext = splitedInput.hasNext();
+            if (hasNext && i < size)
+                timeAndDay[i] = splitedInput.next();
+           
+            else if (hasNext && i == size) 
+            {
+                System.out.println("you entred > " + size + " args.");    
                 System.out.print("-> ");
                 input = scanner.nextLine();
-                if (input.equals(".")) {
+                if (input.equals(".")) 
                     return ;
+                while (input.length() == 0) // if just enter enter
+                {
+                    System.out.println("you entered empty arg");
+                    System.out.print("-> ");
+                    input = scanner.nextLine();
+                    if (input.equals(".")) 
+                        return ;    
                 }
+                splitedInput = new Scanner(input).useDelimiter("\\s+");
+                i = -1;
             }
-            listOfStudents[numberOfStudents] = input;
-             
-            numberOfStudents++;
-            System.out.print("-> ");
-        }
-        if (numberOfStudents == 10) {
-            System.out.println(".");
-        }
-    }
-
-    private static boolean includeEspace(String input) {
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == 32) {
-                return true;
-            }
-        }
-        return false;
-    }
-    private static String[] splitString(String input , int flag) {
-         
-        int wordCount = 1; // at least one word
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == ' ') 
+            else if (!hasNext && i < size) 
             {
-                while (input.charAt(i) == ' ') 
-                    i++;
-                wordCount++;
+                System.out.println("you entred < " + size + " args.");    
+
+                System.out.print("-> ");
+                input = scanner.nextLine();
+                if (input.equals(".")) 
+                    return ;
+                while (input.length() == 0) // if just enter enter
+                {
+                    System.out.println("you entered empty arg");
+                    System.out.print("-> ");
+                    input = scanner.nextLine();
+                    if (input.equals(".")) 
+                        return ;    
+                }
+                splitedInput = new Scanner(input).useDelimiter("\\s+");
+                i = -1;
             }
+
+            i++;
         }
-        if (wordCount == 1) {
-            return null;
-        }        
-        if (flag == 2 && wordCount > 2) 
+        if (size == 2) 
         {
-            return null;
+            strTime = timeAndDay[0];
+            strDay = timeAndDay[1];
         }
-        if (flag == 4 && wordCount > 4) 
+        else
         {
-            return null;
+            nameOfStudent =  timeAndDay[0];
+            strTime = timeAndDay[1];
+            strDate =  timeAndDay[2];
+            strStatus = timeAndDay[3];
         }
-        String[] wordsArray = new String[wordCount];
-
-        int wordStartIndex = 0;
-        int arrayIndex = 0;
-
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == ' '  ) {
-                while (input.charAt(i) == ' ') 
-                    i++;
-                i--;
-                int wordLength = i - wordStartIndex;
-
-                char[] currentWordArray = new char[wordLength];
-                
-                for (int j = 0; j < wordLength; j++) {
-                    currentWordArray[j] = input.charAt(wordStartIndex + j);
-                    }
-
-                wordsArray[arrayIndex] = new String(currentWordArray);
-                arrayIndex++;
-
-                wordStartIndex = i + 1;
-                while (input.charAt(i) == ' ' || input.charAt(i) == 9)
-                    i++; 
-            }
-        }
-
-       int lastWordLength = input.length() - wordStartIndex;
-       char[] lastWordArray = new char[lastWordLength];
-       
-       for (int j = 0; j < lastWordLength; j++) {
-           lastWordArray[j] = input.charAt(wordStartIndex + j);
-       }
-       wordsArray[arrayIndex] = new String(lastWordArray);
-       return wordsArray;
     }
+    
 
+
+
+    private static String getStudentName(int numberOfStudents ,Scanner scanner) 
+    {
+        String input;
+        String studentName;
+
+        
+        studentName = null;
+        System.out.print("-> ");
+        if (!(input = scanner.nextLine()).equals(".") && numberOfStudents < 10)  
+        {
+            while (input.length() == 0) // if just enter enter
+            {
+                System.out.println("you entered empty arg");
+                System.out.print("-> ");
+                input = scanner.nextLine();
+                if (input.equals(".")) 
+                    return null;    
+            }
+            
+            Scanner splitedInput = new Scanner(input).useDelimiter("\\s+");
+            studentName = splitedInput.next();
+            while (splitedInput.hasNext() || studentName.length() > 10 ) // if enter > string or studnet name len > 10
+            {
+                System.out.println("enterd gereter then one arg or student len > 10");
+                System.out.print("-> ");
+                input = scanner.nextLine();
+                if (input.equals(".")) 
+                    return null; 
+                while (input.length() == 0) 
+                {
+                    System.out.println("you entered empty arg");
+                    System.out.print("-> ");
+                    input = scanner.nextLine();
+                    if (input.equals(".")) 
+                        return null; 
+                }
+                splitedInput = new Scanner(input).useDelimiter("\\s+");
+                studentName = splitedInput.next();
+            }            
+        }
+         
+        return studentName;
+    }
+ 
     private static int checkIfDayIsExistInWeek(int date , int time)
     {
+        if (date < 1 || date > 30) 
+        {
+            System.out.println("date should be > 1 and < 30 ");    
+            return -1;
+        }
+        
         if (classes[date - 1][time - 1] == true) 
         {
             return date - 1;     
@@ -360,4 +410,5 @@ public class Program {
         }
         return 0;
     }
+
 }
